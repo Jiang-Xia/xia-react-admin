5aeeec1fcf255fd15f5831dc3c4c26255551bd31
JiangChuanJun <JiangChuanJun@deepaint.cn>
2020-09-17 15:45:12 +0800

    1

--- src/App.js
+++ src/App.js
@@ -1,28 +1,24 @@
-import '@/style/App.scss';
-import '@/style/base.scss';
-import 'antd/dist/antd.css';
-import React from 'react';
-import { HashRouter as Router, Redirect, Route, Switch } from 'react-router-dom';
+import '@/style/App.scss'
+import '@/style/base.scss'
+import 'antd/dist/antd.css'
+import React from 'react'
+import { HashRouter as Router, Redirect, Route, Switch } from 'react-router-dom'
 
 // 路由懒加载
-import loadable from "@/utils/loadable"
+import loadable from '@/utils/loadable'
 const index = loadable(() => import('@/views/index/index'))
 function App() {
-  return (
-    <Router>
-      <Switch>
-
-
-        <Route path='/'
-          exact render={() => <Redirect to='/index' />} />
-        <Route path='/index' component={index} />
-        {/* <Route path='/login' component={Login} />
+    return (
+        <Router>
+            <Switch>
+                <Route path='/' exact render={() => <Redirect to='/index' />} />
+                <Route path='/index' component={index} />
+                {/* <Route path='/login' component={Login} />
             <Route path='/404' component={View404} />
             <Route component={DefaultLayout} /> */}
-      </Switch>
-    </Router>
-
-  );
+            </Switch>
+        </Router>
+    )
 }
 
-export default App;
+export default App
