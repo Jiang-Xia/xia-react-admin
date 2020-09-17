e879763c1f708d91098d45a24667d55dd68497bd
JiangChuanJun <JiangChuanJun@deepaint.cn>
2020-09-17 15:42:21 +0800

    美化

--- src/App.js
+++ src/App.js
@@ -1,21 +1,25 @@
-import React from 'react';
+import '@/style/App.scss';
+import '@/style/base.scss';
 import 'antd/dist/antd.css';
-import '@/style/base.scss'
-import '@/style/App.scss'
-import { HashRouter as Router, Route, Switch, Redirect } from 'react-router-dom'
+import React from 'react';
+import { HashRouter as Router, Redirect, Route, Switch } from 'react-router-dom';
+
 // 路由懒加载
-import loadable from '@/utils/loadable'
-const index = loadable(()=>import('@/views/index/index'))
+import loadable from "@/utils/loadable"
+const index = loadable(() => import('@/views/index/index'))
 function App() {
   return (
     <Router>
-        <Switch>
-            <Route path='/' exact render={() => <Redirect to='/index' />} />
-            <Route path='/index' component={index} />
-            {/* <Route path='/login' component={Login} />
+      <Switch>
+
+
+        <Route path='/'
+          exact render={() => <Redirect to='/index' />} />
+        <Route path='/index' component={index} />
+        {/* <Route path='/login' component={Login} />
             <Route path='/404' component={View404} />
             <Route component={DefaultLayout} /> */}
-        </Switch>
+      </Switch>
     </Router>
 
   );
